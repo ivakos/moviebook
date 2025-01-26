97772deaf48d1c6eed71a6609c9f6fff0152ad02
Ivan Kasenka <kasenka.ivan@gmail.com>
2025-01-26 23:27:10 +0300

    feat: start react app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+import { HashRouter as Router, Route, Routes, useNavigate, useParams } from 'react-router-dom';
+import './styles/normilize.css';
+import './App.css';
+
+
+const App = () => {
+  return (
+    <div className="app">
+      <Router>
+
+      </Router>
+    </div>
+  );
+};
+
+export default App;
